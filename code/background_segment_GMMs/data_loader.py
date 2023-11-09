1a7130bfe68b626a41a58c0206232d89e0afd5ce
vannt263 <nguyenthivan1_t65@hus.edu.vn>
2023-11-09 10:32:19 +0700

    add load_data

--- code/background_segment_GMMs/data_loader.py
+++ code/background_segment_GMMs/data_loader.py
@@ -1,8 +1,7 @@
 from abc import ABC, abstractmethod
 
-
+# Định nghĩa lớp trừu tượng, bắt buộc lớp con phải triển khai phương thức get_next_frame
 class DataLoader(ABC):
-
     @abstractmethod
     def get_next_frame(self):
         pass
