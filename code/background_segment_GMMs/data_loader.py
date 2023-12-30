ecc67e91ebcd704e8977ccb65947092d26cb6711
vannt263 <nguyenthivan1_t65@hus.edu.vn>
2023-12-30 23:01:37 +0700

    remove file

--- code/background_segment_GMMs/data_loader.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC, abstractmethod
-
-# Định nghĩa lớp trừu tượng, bắt buộc lớp con phải triển khai phương thức get_next_frame
-class DataLoader(ABC):
-    @abstractmethod
-    def get_next_frame(self):
-        pass
