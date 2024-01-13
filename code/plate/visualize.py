8afbe34a7cda6445c2b92d2d0ce9d340ab8ee577
Dynjjeo <dynjjeo@gmail.com>
2024-01-13 12:48:16 +0700

    add plate

--- code/plate/visualize.py
+++ code/plate/visualize.py
@@ -5,17 +5,36 @@ import numpy as np
 import pandas as pd
 
 
-results = pd.read_csv('./test_interpolated.csv')
+def draw_border(img, top_left, bottom_right, color=(0, 255, 0), thickness=10, line_length_x=200, line_length_y=200):
+    x1, y1 = top_left
+    x2, y2 = bottom_right
+
+    cv2.line(img, (x1, y1), (x1, y1 + line_length_y), color, thickness)  #-- top-left
+    cv2.line(img, (x1, y1), (x1 + line_length_x, y1), color, thickness)
+
+    cv2.line(img, (x1, y2), (x1, y2 - line_length_y), color, thickness)  #-- bottom-left
+    cv2.line(img, (x1, y2), (x1 + line_length_x, y2), color, thickness)
+
+    cv2.line(img, (x2, y1), (x2 - line_length_x, y1), color, thickness)  #-- top-right
+    cv2.line(img, (x2, y1), (x2, y1 + line_length_y), color, thickness)
+
+    cv2.line(img, (x2, y2), (x2, y2 - line_length_y), color, thickness)  #-- bottom-right
+    cv2.line(img, (x2, y2), (x2 - line_length_x, y2), color, thickness)
+
+    return img
+
+
+results = pd.read_csv('./license_plate_detector.pt')
 
 # load video
 video_path = 'sample.mp4'
 cap = cv2.VideoCapture(video_path)
 
-# fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # Specify the codec
-# fps = cap.get(cv2.CAP_PROP_FPS)
-# width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-# height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-# out = cv2.VideoWriter('./out.mp4', fourcc, fps, (width, height))
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # Specify the codec
+fps = cap.get(cv2.CAP_PROP_FPS)
+width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+out = cv2.VideoWriter('./out.mp4', fourcc, fps, (width, height))
 
 license_plate = {}
 for car_id in np.unique(results['car_id']):
@@ -48,6 +67,10 @@ while ret:
     if ret:
         df_ = results[results['frame_nmr'] == frame_nmr]
         for row_indx in range(len(df_)):
+            # draw car
+            car_x1, car_y1, car_x2, car_y2 = ast.literal_eval(df_.iloc[row_indx]['car_bbox'].replace('[ ', '[').replace('   ', ' ').replace('  ', ' ').replace(' ', ','))
+            draw_border(frame, (int(car_x1), int(car_y1)), (int(car_x2), int(car_y2)), (0, 255, 0), 25,
+                        line_length_x=200, line_length_y=200)
 
             # draw license plate
             x1, y1, x2, y2 = ast.literal_eval(df_.iloc[row_indx]['license_plate_bbox'].replace('[ ', '[').replace('   ', ' ').replace('  ', ' ').replace(' ', ','))
