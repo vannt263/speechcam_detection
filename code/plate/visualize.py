4bf6e2c981dad40a31c783926396c6d934b582bb
Dynjjeo <dynjjeo@gmail.com>
2024-01-14 00:27:25 +0700

    add

--- code/plate/visualize.py
+++ code/plate/visualize.py
@@ -21,17 +21,17 @@ def draw_border(img, top_left, bottom_right, color=(0, 255, 0), thickness=10, li
 
     return img
 
-results = pd.read_csv('D:/study/CV/CK/speechcam_detection/code/plate/test_interpolated.csv')
+results = pd.read_csv('D:/study/CV/CK/speechcam_detection/code/plate/test_interpolated2.csv')
 
 # load video
-video_path = 'D:/study/CV/CK/speechcam_detection/data/video/sample.mp4'
+video_path = 'D:/study/CV/CK/speechcam_detection/data/video/plate1.mp4'
 cap = cv2.VideoCapture(video_path)
 
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # Specify the codec
 fps = cap.get(cv2.CAP_PROP_FPS)
 width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-out = cv2.VideoWriter('D:/study/CV/CK/automatic-number-plate-recognition-python-yolov8-main/out.mp4', fourcc, fps, (width, height))
+out = cv2.VideoWriter('./out1.mp4', fourcc, fps, (width, height))
 
 license_plate = {}
 for car_id in np.unique(results['car_id']):
