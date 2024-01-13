9e368a701443098b1ba80882598e21ac07506225
Dynjjeo <dynjjeo@gmail.com>
2024-01-13 17:32:10 +0700

    add

--- code/plate/util.py
+++ code/plate/util.py
@@ -23,13 +23,6 @@ dict_int_to_char = {'0': 'O',
 
 
 def write_csv(results, output_path):
-    """
-    Write the results to a CSV file.
-
-    Args:
-        results (dict): Dictionary containing the results.
-        output_path (str): Path to the output CSV file.
-    """
     with open(output_path, 'w') as f:
         f.write('{},{},{},{},{},{},{}\n'.format('frame_nmr', 'car_id', 'car_bbox',
                                                 'license_plate_bbox', 'license_plate_bbox_score', 'license_number',
@@ -60,16 +53,8 @@ def write_csv(results, output_path):
         f.close()
 
 
-def license_complies_format(text):
-    """
-    Check if the license plate text complies with the required format.
-
-    Args:
-        text (str): License plate text.
-
-    Returns:
-        bool: True if the license plate complies with the format, False otherwise.
-    """
+def license_format(text):
+# đặt mặc định biển số cho video này 
     if len(text) != 7:
         return False
 
@@ -86,15 +71,6 @@ def license_complies_format(text):
 
 
 def format_license(text):
-    """
-    Format the license plate text by converting characters using the mapping dictionaries.
-
-    Args:
-        text (str): License plate text.
-
-    Returns:
-        str: Formatted license plate text.
-    """
     license_plate_ = ''
     mapping = {0: dict_int_to_char, 1: dict_int_to_char, 4: dict_int_to_char, 5: dict_int_to_char, 6: dict_int_to_char,
                2: dict_char_to_int, 3: dict_char_to_int}
@@ -107,17 +83,8 @@ def format_license(text):
     return license_plate_
 
 
-def read_license_plate(license_plate_crop):
-    """
-    Read the license plate text from the given cropped image.
-
-    Args:
-        license_plate_crop (PIL.Image.Image): Cropped image containing the license plate.
-
-    Returns:
-        tuple: Tuple containing the formatted license plate text and its confidence score.
-    """
-
+def read_plate(license_plate_crop):
+# đọc ký bằng ocr
     detections = reader.readtext(license_plate_crop)
 
     for detection in detections:
@@ -125,23 +92,14 @@ def read_license_plate(license_plate_crop):
 
         text = text.upper().replace(' ', '')
 
-        if license_complies_format(text):
+        if license_format(text):
             return format_license(text), score
 
     return None, None
 
 
 def get_car(license_plate, vehicle_track_ids):
-    """
-    Retrieve the vehicle coordinates and ID based on the license plate coordinates.
-
-    Args:
-        license_plate (tuple): Tuple containing the coordinates of the license plate (x1, y1, x2, y2, score, class_id).
-        vehicle_track_ids (list): List of vehicle track IDs and their corresponding coordinates.
-
-    Returns:
-        tuple: Tuple containing the vehicle coordinates (x1, y1, x2, y2) and ID.
-    """
+# xác định biển số nao là của xe nào 
     x1, y1, x2, y2, score, class_id = license_plate
 
     foundIt = False
